cca032a8ec51f651e86dc0c4b9d4e8c6dc56b10a
Gcat101 <gcatmail2@gmail.com>
2023-06-13 22:33:14 +0300

    Update socials

--- src/components/Socials.svelte
+++ src/components/Socials.svelte
@@ -53,6 +53,13 @@
         },
 
         {
+            "title": "Ko-Fi",
+            "url": "https://ko-fi.com/modernmodpacks",
+            "img": "https://cdn.iconscout.com/icon/free/png-256/free-ko-fi-3521535-2945038.png?f=webp",
+            "splash": false
+        },
+
+        {
             "title": "Twitter",
             "url": "https://twitter.com/ModernModpacks",
             "img": "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjQgNC41NTdjLS44ODMuMzkyLTEuODMyLjY1Ni0yLjgyOC43NzUgMS4wMTctLjYwOSAxLjc5OC0xLjU3NCAyLjE2NS0yLjcyNC0uOTUxLjU2NC0yLjAwNS45NzQtMy4xMjcgMS4xOTUtLjg5Ny0uOTU3LTIuMTc4LTEuNTU1LTMuNTk0LTEuNTU1LTMuMTc5IDAtNS41MTUgMi45NjYtNC43OTcgNi4wNDUtNC4wOTEtLjIwNS03LjcxOS0yLjE2NS0xMC4xNDgtNS4xNDQtMS4yOSAyLjIxMy0uNjY5IDUuMTA4IDEuNTIzIDYuNTc0LS44MDYtLjAyNi0xLjU2Ni0uMjQ3LTIuMjI5LS42MTYtLjA1NCAyLjI4MSAxLjU4MSA0LjQxNSAzLjk0OSA0Ljg5LS42OTMuMTg4LTEuNDUyLjIzMi0yLjIyNC4wODQuNjI2IDEuOTU2IDIuNDQ0IDMuMzc5IDQuNiAzLjQxOS0yLjA3IDEuNjIzLTQuNjc4IDIuMzQ4LTcuMjkgMi4wNCAyLjE3OSAxLjM5NyA0Ljc2OCAyLjIxMiA3LjU0OCAyLjIxMiA5LjE0MiAwIDE0LjMwNy03LjcyMSAxMy45OTUtMTQuNjQ2Ljk2Mi0uNjk1IDEuNzk3LTEuNTYyIDIuNDU3LTIuNTQ5eiIvPjwvc3ZnPg==",
@@ -118,8 +125,8 @@
             gap: 15px;
         }
         a {
-            height: 25px;
-            width: 25px;
+            height: 20px;
+            width: 20px;
         }
     }
 </style>
