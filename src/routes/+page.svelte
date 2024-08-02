757543d6e0d453c63b53eefc771f44ddc7ec525f
Gcat101 <gcatmail2@gmail.com>
2024-08-02 18:35:19 +0300

    ok tizu I did the thing

--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import { goto } from "$app/navigation"
-    import { onMount } from "svelte"
-
-    // Redirect to the default page - projects
-    onMount(() => goto("projects"))
-</script>
