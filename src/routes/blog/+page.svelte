1b69c1601a99dc77656d959c73d8cca90769a2ea
Gcat101 <gcatmail2@gmail.com>
2024-08-25 00:47:13 +0300

    Add under construction warning to blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,9 @@
-dsadas
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+</script>
+
+<div class="w-[100vw] h-[100vh] flex items-center">
+    <span class="w-full flex items-center bg-mm-yellow h-20">
+        <h1 class="w-full h-fit text-center text-black border-y-4 border-black">{$_("ui.construction").toUpperCase()}</h1>
+    </span>
+</div>
