7f53c1e3b48567714a58dbac4e9db442b5354ca8
Gcat101 <gcatmail2@gmail.com>
2024-09-26 17:09:57 +0300

    Privatize keys

--- src/lib/server/serverConsts.ts
+++ src/lib/server/serverConsts.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_DB_URL } from "$env/static/public";
+import { PUBLIC_SUPABASE_DB_URL } from "$env/static/public";
+import { PRIVATE_SUPABASE_ANON_KEY } from "$env/static/private";
 
 const consts = {
-    DB: createClient(PUBLIC_SUPABASE_DB_URL, PUBLIC_SUPABASE_ANON_KEY)
+    DB: createClient(PUBLIC_SUPABASE_DB_URL, PRIVATE_SUPABASE_ANON_KEY)
 }
 
 export default consts
--- src/routes/api/exchangeGithubCode/+server.ts
+++ src/routes/api/exchangeGithubCode/+server.ts
@@ -1,9 +1,10 @@
-import { PUBLIC_GITHUB_CLIENT_ID, PUBLIC_GITHUB_CLIENT_SECRET } from "$env/static/public";
+import { PRIVATE_GITHUB_CLIENT_SECRET } from "$env/static/private";
+import { PUBLIC_GITHUB_CLIENT_ID } from "$env/static/public";
 import type { RequestHandler } from "./$types";
 
 // Exchange github temporary code for token, used for blog github oauth2
 export const GET: RequestHandler = async ({ url }) => {
-    return new Response(await (await fetch(`https://github.com/login/oauth/access_token?client_id=${PUBLIC_GITHUB_CLIENT_ID}&client_secret=${PUBLIC_GITHUB_CLIENT_SECRET}&code=${url.searchParams.get("code")}`, {
+    return new Response(await (await fetch(`https://github.com/login/oauth/access_token?client_id=${PUBLIC_GITHUB_CLIENT_ID}&client_secret=${PRIVATE_GITHUB_CLIENT_SECRET}&code=${url.searchParams.get("code")}`, {
         method: "POST",
         headers: {'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded'}
     })).text())
